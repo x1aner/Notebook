672e0857f7fd52a0fea1917d0068e271bb3b7765
X1aner <wusrong@foxmail.com>
2023-07-18 01:27:44 +0800

    new tcl file

---
+++ helloworld.tcl
@@ -0,0 +1,9 @@
+############################################################
+# Author: 54r
+# Date: 2023-07-18 01:27:11
+# LastEditTime: 2023-07-18 01:27:28
+# Description: 
+# 
+# Copyright (c) 2023 by MAIXISOFTWARE, All Rights Reserved. 
+############################################################
+puts "helloworld"
