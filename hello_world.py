ee5e9efb3fce99509d95f31bd4d083fdfd0f6196
X1aner <wusrong@foxmail.com>
2023-07-18 01:18:55 +0800

    change hello world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print "Hello World!"
+'''
+Author: 54r
+Date: 2023-07-17 23:53:16
+LastEditTime: 2023-07-18 01:18:11
+Description: 
+
+Copyright (c) 2023 by MAIXISOFTWARE, All Rights Reserved. 
+'''
+print ("Hello World!")
