f7e929e00e2e7077c458d146a33b5eeae638d3ac
Xianer <wusrong@foxmail.com>
2023-12-28 17:25:58 +0800

    新增单元选择

--- helloworld.tcl
+++ helloworld.tcl
@@ -1,9 +1,11 @@
 ############################################################
 # Author: 54r
 # Date: 2023-07-18 01:27:11
-# LastEditTime: 2023-07-18 01:27:28
+# LastEditTime: 2023-12-28 17:25:21
 # Description: 
 # 
 # Copyright (c) 2023 by MAIXISOFTWARE, All Rights Reserved. 
 ############################################################
 puts "helloworld"
+
+hm_createmarkpanel elems 1 "请选择单元"
